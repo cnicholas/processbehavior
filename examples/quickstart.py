197bdc658eb7493df2810d81ca4c98fad2055347
cnicholas <chris.nicholas@gmail.com>
2025-10-06 18:49:30 -0400

    do over

--- examples/quickstart.py
+++
@@ -1,6 +0,0 @@
-from processbehavior import AnalysisSpec, analyze, load_demo
-
-df = load_demo()
-spec = AnalysisSpec(response_var='y', time_var='t', grouping=['line'])
-res = analyze(df, spec)
-print(res['charts'].keys())
